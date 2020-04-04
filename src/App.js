e8f4110c1620eaea74977c2b6a51ac59c4182072
KangG <dmssk13@gmail.com>
2020-04-04 17:33:29 +0900

    초기세팅 및 기본 페이지 구축

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+import Header from './components/Header';
+import Contents from './components/Contents';
+
+const App = () => (
+  <Fragment>
+  
+    <Header />
+    
+    <main className="my-5 py-5">
+      <Container className="px-0">
+        <Row noGutters className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative">
+
+          <Col className="py-5 mb-5 py-md-0 mb-md-0">
+            <Contents />
+          </Col>
+          
+        </Row>
+      </Container>
+    </main>
+    
+  </Fragment>
+);
+
+export default App;
---
+++ src/components/Contents.js
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from 'react';
+import { Badge } from 'reactstrap';
+
+class Contents extends Component {
+  
+  render() {
+    return (
+      <Fragment>
+        <div className="position-relative">
+        
+          <span className="d-block pb-2 mb-0 h6 text-uppercase text-info font-weight-bold">
+            Project 1
+            <Badge pill color="success" className="text-uppercase px-2 py-1 ml-3 mb-1 align-middle" style={{ fontSize: '0.75rem' }}>New</Badge>
+          </span>
+          
+          <span className="d-block pb-4 h2 text-dark border-bottom border-gray">React Project</span>
+          
+          <article className="pt-5 text-secondary text-justify" style={{ fontSize: '0.9rem', whiteSpace: 'pre-line' }}>
+            React를 이용한 포트플리오 프로젝트입니다.
+          </article>
+          
+        </div>
+      </Fragment>
+    );
+  }
+  
+}
+
+export default Contents;
